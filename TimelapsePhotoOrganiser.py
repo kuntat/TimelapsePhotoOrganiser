bdf1efcefc2611a4dd2ea98ab9af0738dcd4212f
kuntat <kuntat@users.noreply.github.com>
2016-05-15 22:28:20 +0800

    First commit

---
+++ TimelapsePhotoOrganiser.py
@@ -0,0 +1,47 @@
+##
+## TimelapsePhotoOrganiser.py
+## TimelapsePhotoOrganiser
+##
+## Created by Kun Tat on 14/5/16.
+## Copyright 2016 Kun Tat. All rights reserved.
+##
+
+
+from os import listdir, makedirs
+from os.path import isfile, join, getmtime, isdir, exists
+from time import ctime
+from shutil import move
+import errno
+
+def mkdir_p(path):
+    try:
+        makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and isdir(path):
+            pass
+        else:
+            raise        
+
+##mypath = "/Users/kuntat/Desktop/new"
+mypath = raw_input("Enter directory: ")
+assert exists(mypath), "I did not find the file at, "+str(mypath)
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and not f.startswith('.')]
+
+
+foldernumber = 1
+lastmod = getmtime(join(mypath, onlyfiles[0]))
+mkdir_p(mypath + "/Timelapse%s" %foldernumber)
+
+print "Moving your files for you..."
+
+for f in onlyfiles:
+    f = join(mypath, f)
+    if (getmtime(f) - lastmod < 60.0):
+        lastmod = getmtime(f)
+    else:
+        lastmod = getmtime(f)
+        foldernumber += 1
+        mkdir_p(mypath + "/Timelapse%s" %foldernumber)
+    move(f, mypath + "/Timelapse%s" %foldernumber) 
+
+print "Done!"
